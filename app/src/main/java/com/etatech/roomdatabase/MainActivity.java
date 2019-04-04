7e0be37e88c9ad16120340f56391591424e800a2
Fady <fady.fouad.a@gmail.com>
2019-04-04 16:59:15 +0200

    Create Main Fragment

--- app/src/main/java/com/etatech/roomdatabase/MainActivity.java
+++ app/src/main/java/com/etatech/roomdatabase/MainActivity.java
@@ -1,13 +1,27 @@
 package com.etatech.roomdatabase;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static FragmentManager manager ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
+
+        if (findViewById(R.id.fragment_container)!=null){
+            if (savedInstanceState!=null){
+                return;
+            }
+            manager.beginTransaction()
+                    .add(R.id.fragment_container,new MainFragment())
+                    .commit();
+        }
     }
 }
