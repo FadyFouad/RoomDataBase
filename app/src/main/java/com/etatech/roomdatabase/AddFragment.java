54f4592add73dfa61d281471f4e9ca24f13f6a22
Fady <fady.fouad.a@gmail.com>
2019-04-04 18:00:35 +0200

    Update Add User Fragment

--- app/src/main/java/com/etatech/roomdatabase/AddFragment.java
+++ app/src/main/java/com/etatech/roomdatabase/AddFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +26,24 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        final TextView nameTV = view.findViewById(R.id.edt_txt_name);
+        final TextView emailTV = view.findViewById(R.id.edt_txt_email);
+        Button saveBtn = view.findViewById(R.id.btn_save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameTV.getText().toString();
+                String email = emailTV.getText().toString();
+
+
+                User user = new User(name,email);
+                MainActivity.usersDataBase.userDao().insert(user);
+                Toast.makeText(getContext(), "User Added", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/etatech/roomdatabase/MainActivity.java
+++ app/src/main/java/com/etatech/roomdatabase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.etatech.roomdatabase;
 
+import android.arch.persistence.room.Room;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static FragmentManager manager ;
+    public static UsersDataBase usersDataBase ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         manager = getSupportFragmentManager();
+        usersDataBase = Room.databaseBuilder(this,UsersDataBase.class,"user_database")
+                .allowMainThreadQueries() //TODO : Solve Cannot access database on the main thread since it may potentially lock the UI for a long period of time
+                .build();
 
         if (findViewById(R.id.fragment_container)!=null){
             if (savedInstanceState!=null){
